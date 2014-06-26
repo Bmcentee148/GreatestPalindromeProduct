85b60458d8b23fd5c6bfad603d78abb4a3c22955
Brian McEntee <brian.m.mcentee@gmail.com>
2014-06-26 15:21:48 -0400

    Added method to find greatest palindrome.

--- PalindromeProduct.java
+++ PalindromeProduct.java
@@ -3,11 +3,14 @@
 	999 * 999. */
 
 class PalindromeProduct{
-
+	public static final int START = 100;
+	public static final int MAX = 999;
 	public static void main(String [] args){
-		System.out.println(isPalindrome(2120212));
+		/*System.out.println(isPalindrome(2120212));
 		System.out.println(isPalindrome(5));
-		System.out.println(isPalindrome(300020));
+		System.out.println(isPalindrome(300020));*/
+
+
 
 	}
 
@@ -17,5 +20,17 @@ class PalindromeProduct{
 		String numBackward = temp.reverse().toString();
 
 		return numForward.equals(numBackward);
+	}
+
+	public static int CalcGreatestPalindrome(){
+		int greatest = 0;
+		int product = 0;
+		for(int i = START; i <= MAX; i ++){
+			for(int j = i; j <= MAX; j++){
+				product = i * j;
+				if(isPalindrome(product)) greatest = product;
+			}
+		}
+		return greatest;
 	}
 }
